dbdcb368d931943c77c8e9bfc24f6d0706995447
Ian W. Scott <iscott@tyndale.ca>
2011-11-24 16:07:19 -0500

    first commit

---
+++ models/menu.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+#########################################################################
+## Customize your APP title, subtitle and menus here
+#########################################################################
+
+response.title = request.application
+response.subtitle = T('A tool for managing primary-text research in the humanities')
+
+#http://dev.w3.org/html5/markup/meta.name.html
+response.meta.author = 'Ian W. Scott'
+response.meta.description = 'A web-app for managing primary-text research in the humanities.'
+response.meta.keywords = 'research, humanities, biblical'
+response.meta.generator = 'Web2py Enterprise Framework'
+response.meta.copyright = 'Copyright 2011'
+
+
+response.menu = [
+    (T('Home'), False, URL('default','index'), [])
+    ]
+
+response.menu+=[
+    (T('This App'), False, URL('admin', 'default', 'design/%s' % request.application),
+     [
+            (T('Controller'), False,
+             URL('admin', 'default', 'edit/%s/controllers/%s.py' \
+                     % (request.application,request.controller=='appadmin' and
+                        'default' or request.controller))),
+            (T('View'), False,
+             URL('admin', 'default', 'edit/%s/views/%s' \
+                     % (request.application,response.view))),
+            (T('Layout'), False,
+             URL('admin', 'default', 'edit/%s/views/layout.html' \
+                     % request.application)),
+            (T('Stylesheet'), False,
+             URL('admin', 'default', 'edit/%s/static/base.css' \
+                     % request.application)),
+            (T('DB Model'), False,
+             URL('admin', 'default', 'edit/%s/models/db.py' \
+                     % request.application)),
+            (T('Menu Model'), False,
+             URL('admin', 'default', 'edit/%s/models/menu.py' \
+                     % request.application)),
+            (T('Database'), False,
+             URL(request.application, 'appadmin', 'index')),
+
+            (T('Errors'), False,
+             URL('admin', 'default', 'errors/%s' \
+                     % request.application)),
+
+            (T('About'), False,
+             URL('admin', 'default', 'about/%s' \
+                     % request.application)),
+
+            ]
+   )]
